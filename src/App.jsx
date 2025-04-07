f33abdedcfef0522b7c630a2e91232006bdcb054
Nightingale2014 <topoftoppark@naver.com>
2025-04-08 01:20:32 +0900

    🧼 .gitignore 추가 및 초기 커밋

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MdPickSection from "./components/MdPickSection";
+
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <MdPickSection />
+    </div>
+  );
+}
+
+export default App;
